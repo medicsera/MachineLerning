0b5208b24685cf622a2905f498b6812727f84480
medicsera <anton.mirko.0550@gmail.com>
2025-09-11 13:07:36 +0700

    complete task 1

---
+++ task_1/task_1.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+"""
+Напишите программы, которые создают следующие массивы NumPy:
+1. Массив со значениями 1, 7, 13, 105. Посчитайте и выведите на экран размер
+памяти, который он занимает. Сохраните массив в текстовый и бинарный файлы.
+Затем загрузите его и выведите на экран.
+"""
+
+def task_1():
+    arr = np.array([1,7,13,105])
+
+    print(f"Массив: {arr}")
+    print(f"Размер памяти: {arr.nbytes}")
+
+    np.savetxt("task_1/arr.txt",arr, fmt="%d")
+
+    np.save("task_1/arr.npy", arr)
+
+    load_txt = np.loadtxt('task_1/arr.txt', dtype=np.int32)
+    print(f"Загруженный текстовый: {load_txt}")
+
+    load_bin = np.load('task_1/arr.npy')
+    print(f"Загруженный бинарный: {load_bin}")
+
+
+task_1()
