ef0128accd33d47405c646f37090e8aea29da1f9
medicsera <anton.mirko.0550@gmail.com>
2025-09-13 21:51:24 +0700

    done task 6 num 3

--- task_6.py
+++ task_6.py
@@ -36,4 +36,28 @@ def num_2():
 
     plt.show()
 
-num_2()
+
+#num_2()
+
+def num_3():
+    x1 = np.linspace(0,100,100)
+    y1 = 2 * x1
+    
+    y2 = np.linspace(0,10000,100)
+    x2 = np.sqrt(y2)
+
+    fig, axs = plt.subplots(1,2, figsize=(12,4))
+
+    axs[0].plot(x1,y1,color="blue",linewidth=3)
+    axs[0].set_xlabel("x")
+    axs[0].set_ylabel("y")
+
+    axs[1].plot(x2,y2,color="red", linestyle="dashdot", linewidth=3)
+    axs[1].set_xlabel("x")
+    axs[1].set_ylabel("y")
+
+    plt.tight_layout()
+    plt.show()
+
+
+num_3()
