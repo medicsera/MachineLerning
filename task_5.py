44a21d55d55dfc71be453941f7b0c50fd65f75f8
medicsera <anton.mirko.0550@gmail.com>
2025-09-13 17:07:26 +0700

    done task 5 num 4

--- task_5.py
+++ task_5.py
@@ -60,4 +60,38 @@ def num_3():
     plt.show()
 
 
-num_3()
+#num_3()
+
+def num_4():
+    x = ['Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++']
+    popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
+    x_pos = [i for i, _ in enumerate(x)]
+
+    fig, ax = plt.subplots()
+    rect = ax.bar(x_pos, popularity, color="blue")
+    
+    plt.xlabel("Languages")
+    plt.ylabel("Popularity")
+    plt.title("PopularitY of Programming Language\n" + "Worldwide, Oct 2017 compared to a year ago")
+    plt.xticks(x_pos, x)
+
+    plt.minorticks_on()
+    plt.grid(which='major', linestyle='-', linewidth='0.5', color='red')
+    plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+
+    def label(rects):
+        for rect in rects:
+            height = rect.get_height()
+            ax.text(rect.get_x() + rect.get_width()/2.,
+                    1.05*height,
+                    '%f' % float(height),
+                    ha="center", 
+                    va="bottom")
+
+
+    label(rect)
+
+    plt.show()
+
+
+num_4()
