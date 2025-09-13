ef588ce3e8338abe77ed69de4d50d6221bbbf2f0
medicsera <anton.mirko.0550@gmail.com>
2025-09-13 16:36:29 +0700

    done task 5 num 2

--- task_5.py
+++ task_5.py
@@ -20,4 +20,25 @@ def num_1():
     plt.show()
 
 
-num_1()
+#num_1()
+
+def num_2():
+    x = ['Java', 'Python', 'PHP', 'JS', 'C#', 'C++']
+    popularity = [22.2, 17.5, 8.8, 8, 7.7, 6.7]
+    x_pos = [i for i, _ in enumerate(x)]
+
+    plt.barh(x_pos, popularity, color='green')
+    plt.xlabel("Popularity")
+    plt.ylabel("Languages")
+    plt.title("PopularitY of Programming Language\n" + "Worldwide, Oct 2017 compared to a year ago")
+    plt.yticks(x_pos, x)
+
+    plt.minorticks_on()
+    plt.grid(which='major', linestyle='-', linewidth='0.5', color='red')
+    plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+
+    plt.show()
+
+
+
+num_2()    
